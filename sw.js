f876a78c69b4444898c9c3e390c3aae7b0a67f3b
CVPyM <mejoraproyectos0@gmail.com>
2026-01-12 12:28:13 -0600

    Descripción de los cambios realizados

--- sw.js
+++ sw.js
@@ -1,15 +1,36 @@
+const CACHE_NAME = 'cv-barcode-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-const CACHE_NAME = 'cv-barcode-v1';
-const ASSETS = ['./', './index.html'];
-
+// Instalación: Cachear archivos críticos
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activación: Limpiar caches viejos
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
+// Fetch: Requerido para que Chrome considere la App como PWA instalable
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
